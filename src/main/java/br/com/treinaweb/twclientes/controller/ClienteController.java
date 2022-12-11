1abf37b713784438ced3fd1a903309356cfa58d6
Luan Kenzley <kenzleydev@outlook.com>
2022-12-11 19:16:37 -0300

    adicionando arquivos estáticos

--- src/main/java/br/com/treinaweb/twclientes/controller/ClienteController.java
+++ src/main/java/br/com/treinaweb/twclientes/controller/ClienteController.java
@@ -63,6 +63,23 @@ public class ClienteController {
 
         ModelAndView modelAndView = new ModelAndView("redirect:/cliente");
         clienteRepository.save(cliente);
+        return modelAndView;
+    }
+
+    @GetMapping("/{id}/editar")
+    public ModelAndView editar(@PathVariable Long id) {
+
+        ModelAndView modelAndView = new ModelAndView("cliente/edicao");
+        Optional<Cliente> cliente = clienteRepository.findById(id);
+        modelAndView.addObject("cliente", cliente.get());
+
+        return modelAndView;
+    }
+
+    @PostMapping("/{id}/editar")
+    public ModelAndView editar(Cliente cliente) {
+        ModelAndView modelAndView = new ModelAndView("redirect:/cliente");
+        clienteRepository.save(cliente);
 
         return modelAndView;
     }
